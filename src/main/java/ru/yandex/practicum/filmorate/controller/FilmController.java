6b32cea975bba5c974ba93ec1a9c27e8aff2404a
iRiveri <rivers-property@yandex.ru>
2024-03-03 19:43:45 +0300

    feat: new spring file controller

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,26 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.HashMap;
 
 @RestController
 public class FilmController {
+    private final HashMap<Integer,Film> filmMap = new HashMap<>();
+    @PostMapping("/film")
+    public Film addFilm(@RequestBody Film film) {
+        filmMap.put(film.getId(),film);
+        return film;
+    }
+
+    @PutMapping("/film")
+    public Film updateFilm(@RequestBody Film film) {
+        filmMap.put(film.getId(),film);
+        return film;
+    }
+    @GetMapping("/film")
+    public HashMap<Integer,Film> getAllFilms() {
+        return filmMap;
+    }
 }
