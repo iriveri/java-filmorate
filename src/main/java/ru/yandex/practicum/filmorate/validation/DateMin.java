21a39a60d240cb9fa76df4e2ab275b00c21b16a7
iRiveri <rivers-property@yandex.ru>
2024-03-05 18:09:34 +0300

    refactor: small fix

--- src/main/java/ru/yandex/practicum/filmorate/validation/DateMin.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/DateMin.java
@@ -7,7 +7,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 @Constraint(validatedBy = DateMinValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
