4c69b6323b0eb41fcbdad99b0ce13f094d576863
iRiveri <rivers-property@yandex.ru>
2024-03-17 16:16:19 +0300

    feat/refactor: services updated to new functionality

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,26 +15,40 @@ public class FilmService {
 
     FilmStorage storage;
 
-    public void addFilm(Film newFilm){
+    public void addFilm(Film newFilm) {
         storage.addFilm(newFilm);
     }
-    public void updateFilm(Film existingFilm){
+
+    public void updateFilm(Film existingFilm) {
         storage.updateFilm(existingFilm);
     }
-    public Film getFilm(int id)
-    {
+
+    public Film getFilm(long id) {
         return storage.getFilm(id);
     }
-    public void deleteFilm(int id){
+
+    public void deleteFilm(long id) {
         storage.deleteFilm(id);
     }
-    public void likeFilm(int id){
 
+    public void likeFilm(long filmId, long userId) {
+        var film = storage.getFilm(filmId);
+        film.addLike(userId);
     }
-    public void dislikeFilm(int id){
 
+    public void removeLike(long filmId, long userId) {
+        var film = storage.getFilm(filmId);
+        film.removeLike(userId);
     }
-    public List<Film> getTenMostLikedFilms(){
-        return List.of();
+
+    public List<Film> getPopularFilms(int size) {
+        List<Film> allFilms = storage.toList();
+
+        // Сортируем фильмы по количеству лайков в убывающем порядке
+        allFilms.sort(Comparator.comparingInt(Film::getLikesSize).reversed());
+
+        return allFilms.stream()
+                .limit(size)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,23 +13,55 @@ import java.util.List;
 public class UserService {
     UserStorage storage;
 
-    public void addUser(User newUser){
+    public void addUser(User newUser) {
+        if (newUser.getName() == null) {
+            newUser.setName(newUser.getLogin());
+        }
         storage.addUser(newUser);
     }
-    public void updateUser(User existingUser){
+
+    public void updateUser(User existingUser) {
         storage.updateUser(existingUser);
     }
-    public User getUser(int id)
-    {
-        return storage.getUser(id);
-    }
-    public void deleteUser(int id){
-        storage.deleteUser(id);
-    }
-    public void addFriend(int id){
 
+    public User getUser(long userId) {
+        return storage.getUser(userId);
     }
-    public List<User> commonFriends(){
-        return List.of();
+
+    public void deleteUser(long userId) {
+        var user = storage.getUser(userId);
+
+        for(var friend : user.getFriends()){
+            friend.removeFriend(user);
+        }
+        storage.deleteUser(userId);
+    }
+
+    public void addFriend(long userId, long friendId) {
+        var user = storage.getUser(userId);
+        var friend = storage.getUser(friendId);
+
+        user.addFriend(friend);
+        //@todo temporary solution
+        friend.addFriend(user);
+    }
+    public void removeFriend(long userId, long friendId) {
+        var user = storage.getUser(userId);
+        var friend = storage.getUser(friendId);
+
+        user.removeFriend(friend);
+        friend.removeFriend(user);
+    }
+    public List<User> getFriends(long userId) {
+        return storage.getUser(userId).getFriends();
+    }
+    public List<User> getCommonFriends(long firstUserId, long secondUserId) {
+        var firstFriendsList = getFriends(firstUserId);
+        var secondFriendsList = getFriends(secondUserId);
+
+        List<User> common = new ArrayList<>(firstFriendsList);
+        common.retainAll(secondFriendsList);
+
+        return common;
     }
 }
