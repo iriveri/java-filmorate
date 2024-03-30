fca97163363f0b91b213afd0a47ee710c41ec261
iRiveri <rivers-property@yandex.ru>
2024-03-30 10:43:08 +0300

    feat: Services adapted to new interfaces

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,61 +1,67 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.GenreStorage;
+import ru.yandex.practicum.filmorate.storage.LikeStorage;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class FilmService {
-    private final FilmStorage storage;
+    private final FilmStorage filmStorage;
+    private final GenreStorage genreStorage;
+    private final LikeStorage likeStorage;
+
 
     @Autowired
-    public FilmService(FilmStorage storage) {
-        this.storage = storage;
+
+    public FilmService(@Qualifier("DbFilmStorage") FilmStorage storage,
+                       @Qualifier("DbGenreStorage") GenreStorage genreStorage,
+                       @Qualifier("DbLikeStorage") LikeStorage likeStorage) {
+        this.filmStorage = storage;
+        this.genreStorage = genreStorage;
+        this.likeStorage = likeStorage;
     }
 
     public void addFilm(Film newFilm) {
-        storage.addFilm(newFilm);
+        filmStorage.addFilm(newFilm);
     }
 
     public void updateFilm(Film existingFilm) {
-        storage.updateFilm(existingFilm);
+        filmStorage.updateFilm(existingFilm);
     }
 
     public Film getFilm(long id) {
-        return storage.getFilm(id);
+        return filmStorage.getFilm(id);
     }
 
     public List<Film> getAllFilms() {
-        return storage.toList();
+        return filmStorage.toList();
     }
 
     public void deleteFilm(long id) {
-        storage.deleteFilm(id);
+        filmStorage.deleteFilm(id);
     }
 
     public void likeFilm(long filmId, long userId) {
-        var film = storage.getFilm(filmId);
-        film.addLike(userId);
+        likeStorage.addLike(filmId,userId);
     }
 
     public void removeLike(long filmId, long userId) {
-        var film = storage.getFilm(filmId);
-        film.removeLike(userId);
+        likeStorage.removeLike(filmId,userId);
     }
 
     public List<Film> getPopularFilms(int size) {
-        List<Film> allFilms = storage.toList();
 
-        // Сортируем фильмы по количеству лайков в убывающем порядке
-        allFilms.sort(Comparator.comparingInt(Film::getLikesSize).reversed());
+        List<Long> filmsId = likeStorage.getPopularFilmsId(size);
 
-        return allFilms.stream()
-                .limit(size)
+        return filmsId.stream()
+                .map(this::getFilm)
                 .collect(Collectors.toList());
     }
 }
