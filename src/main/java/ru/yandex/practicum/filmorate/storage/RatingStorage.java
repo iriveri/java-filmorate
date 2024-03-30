2e3319f216e70b093ae8ae3931605d2ad6a3292a
iRiveri <rivers-property@yandex.ru>
2024-03-31 02:26:18 +0300

    feat: small fix)

--- src/main/java/ru/yandex/practicum/filmorate/storage/RatingStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/RatingStorage.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.MpaRating;
 
-import java.util.Arrays;
 import java.util.List;
 
 public interface RatingStorage {
